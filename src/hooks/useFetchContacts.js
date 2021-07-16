8fa94957e1545fd2772dada0a847f009fb33bc92
Noman <numansafi97@gmail.com>
2021-07-16 19:56:38 +0500

    pushh

--- src/hooks/useFetchContacts.js
+++ src/hooks/useFetchContacts.js
@@ -4,13 +4,13 @@ const token = "059c420e-7424-431f-b23b-af0ecabfe7b8";
 export default function useFetchCategories() {
   const data = useQuery("contacts", () =>
     axios
-      .get(`https://api-im.chatdaddy.tech/contacts`, {
+      .get("https://api-im.chatdaddy.tech/contacts", {
         headers: {
           "Content-Type": "application/json",
           Authorization: `Bearer ${token}`,
         },
       })
-      .then((res) => console.log("res", res))
+      .then((res) => res.data)
   );
   return data;
 }
